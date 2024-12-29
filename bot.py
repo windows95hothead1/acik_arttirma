5fb53dc4c99559ae68cf8f271224fea6401b6245
GitKodland <125306926+GitKodland@users.noreply.github.com>
2024-12-29 15:53:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,17 +13,17 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 manager = DatabaseManager(DATABASE)
 manager.create_tables()
 
-# A command for user registration
+# Kullanıcı kaydı için bir komut
 @bot.command()
 async def start(ctx):
     user_id = ctx.author.id
     if user_id in manager.get_users():
-        await ctx.send("You are registered already!")
+        await ctx.send("Zaten kayıtlısınız!")
     else:
         manager.add_user(user_id, ctx.author.name)
-        await ctx.send("""Hi! Welcome! You have been successfully registered! You'll be receiving new images every minute, and you'll have a chance to get them! To do this, you need to click on the 'Get!' button! Only the first three users to click the 'Get!' button will get the picture! =)""")
+        await ctx.send("""Merhaba! Hoş geldiniz! Başarılı bir şekilde kaydoldunuz! Her dakika yeni resimler alacaksınız ve bunları elde etme şansınız olacak! Bunu yapmak için “Al!” butonuna tıklamanız gerekiyor! Sadece “Al!” butonuna tıklayan ilk üç kullanıcı resmi alacaktır! =)""")
 
-# A scheduled task for sending images
+# Resim göndermek için zamanlanmış bir görev
 @tasks.loop(minutes=1)
 async def send_message():
     for user_id in manager.get_users():
@@ -37,7 +37,7 @@ async def send_message():
 async def send_image(user, image_path, prize_id):
     with open(image_path, 'rb') as img:
         file = discord.File(img)
-        button = discord.ui.Button(label="Get!", custom_id=str(prize_id))
+        button = discord.ui.Button(label="Al!", custom_id=str(prize_id))
         view = discord.ui.View()
         view.add_item(button)
         await user.send(file=file, view=view)
@@ -51,13 +51,13 @@ async def on_interaction(interaction):
         if manager.add_winner(user_id, custom_id):
             with open(f'img/{img}', 'rb') as photo:
                 file = discord.File(photo)
-                await interaction.response.send_message(file=file, content="Congratulations, you got the image!")
+                await interaction.response.send_message(file=file, content="Tebrikler, resmi aldınız!")
         else:
-            await interaction.response.send_message(content="Unfortunately, someone has already claimed this image.", ephemeral=True)
+            await interaction.response.send_message(content="Maalesef, bu resmi bir başkası çoktan aldı.", ephemeral=True)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}!')
+    print(f'{bot.user} olarak giriş yapıldı!')
     if not send_message.is_running():
         send_message.start()
 
